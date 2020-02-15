0aa5a64a8351c07c5672949e0f82918b1bdc60b9
javad <javadbat@gmail.com>
2020-02-15 14:54:16 +0330

    move game to seprate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sudoko/screens/game/game_store.dart';
-import 'package:sudoko/widgets/header/header.dart';
-import 'package:sudoko/widgets/keypad/keypad.dart';
-import './widgets/cell/cell_store.dart';
-import './widgets/board/board.dart';
+import 'package:sudoko/screens/game/game.dart';
 
-final cellStore = CellStore(0, 0, 0, 0);
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -19,64 +14,4 @@ class MyApp extends StatelessWidget {
       home: MyHomePage(),
     );
   }
-}
-
-final gameStore = new GameStore();
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: null,
-      backgroundColor: Color(0xFFfdfdfd),
-      body: SafeArea(
-        child: Center(
-            child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-              Header(),
-              Flexible(
-                child: AspectRatio(
-                  child: Board(gameStore.boardStore),
-                  aspectRatio: 1,
-                ),
-                flex: 2,
-              ),
-              Flexible(
-                  child: Keypad(
-                onNumberKeyTapped: this.onNumberKeyTapped,
-                onEraseKeyTapped: this.onEraseKeyTapped,
-                onHintKeyTapped: this.onHintKeyTapped,
-                onNoteKeyTapped: this.onNotesKeyTapped,
-                onResetKeyTapped: this.onResetKeyTapped,
-                onUndoKeyTapped: this.onUndoKeyTapped,
-              )),
-            ],
-          ),
-        )),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-
-  onNumberKeyTapped(int value) {
-    if (gameStore.boardStore.isNoteMode) {
-      gameStore.boardStore.toggleSelectedCellNote(value);
-    } else {
-      gameStore.boardStore.editSelectedCellValue(value);
-    }
-  }
-
-  onResetKeyTapped() {}
-  onHintKeyTapped() {}
-  onEraseKeyTapped() {
-    //todo check if the vaalue is not initialized by app
-    this.onNumberKeyTapped(0);
-  }
-
-  onNotesKeyTapped() {
-    gameStore.boardStore.toggleNoteMode();
-  }
-
-  onUndoKeyTapped() {}
 }
---
+++ lib/screens/game/game.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:sudoko/widgets/board/board.dart';
+import 'package:sudoko/widgets/header/header.dart';
+import 'package:sudoko/widgets/keypad/keypad.dart';
+import 'game_store.dart';
+
+final gameStore = new GameStore();
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      backgroundColor: Color(0xFFfdfdfd),
+      body: SafeArea(
+        child: Center(
+            child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Header(),
+              Flexible(
+                child: AspectRatio(
+                  child: Board(gameStore.boardStore),
+                  aspectRatio: 1,
+                ),
+                flex: 2,
+              ),
+              Flexible(
+                  child: Keypad(
+                onNumberKeyTapped: this.onNumberKeyTapped,
+                onEraseKeyTapped: this.onEraseKeyTapped,
+                onHintKeyTapped: this.onHintKeyTapped,
+                onNoteKeyTapped: this.onNotesKeyTapped,
+                onResetKeyTapped: this.onResetKeyTapped,
+                onUndoKeyTapped: this.onUndoKeyTapped,
+              )),
+            ],
+          ),
+        )),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  onNumberKeyTapped(int value) {
+    if (gameStore.boardStore.isNoteMode) {
+      gameStore.boardStore.toggleSelectedCellNote(value);
+    } else {
+      gameStore.boardStore.editSelectedCellValue(value);
+    }
+  }
+
+  onResetKeyTapped() {}
+  onHintKeyTapped() {}
+  onEraseKeyTapped() {
+    //todo check if the vaalue is not initialized by app
+    this.onNumberKeyTapped(0);
+  }
+
+  onNotesKeyTapped() {
+    gameStore.boardStore.toggleNoteMode();
+  }
+
+  onUndoKeyTapped() {}
+}
